0da69b31ca6b910b1c40ba7306805001bb6655fd
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-04-27 16:07:50 -0500

    ExpenseItem Format Date Update
    • Formatted the Date in ExpenseItem to be more Readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,19 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
   const expenseDate = props.date
+  const expenseMonth = expenseDate.toLocaleString('en-US', { month: 'long' })
+  const expenseYear = expenseDate.toLocaleString('en-US', { day: '2-digit' })
+  const expenseDay = expenseDate.getFullYear()
   const expenseTitle = props.title
   const expenseAmount = props.amount
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDay}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>${expenseAmount}</div>
