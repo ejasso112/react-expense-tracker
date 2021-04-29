35cea27837ba636035492aa96a7b8143cfcc51d3
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-04-29 13:08:59 -0500

    Dynamic Filtering, Storing and Rendering Expenses
    • Expense array is now stored in state.
    • Can now add new expenses to expense array.
    • Filter by year is now integrated.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,13 +13,12 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear)
   }
 
+  const expensesMap = expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear).map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
+
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      {expensesMap}
     </Card>
   )
 }
